da3788d2ce6c809750e8b28facbecb9966b661e9
Patrick Siemen <post@patrick-siemen.de>
2023-06-06 14:56:32 +0200

    improved the checkout converter to output an xml order file using the symfony serializer

--- demo/symfony/src/Commerce/Converter/CheckoutConverter.php
+++ demo/symfony/src/Commerce/Converter/CheckoutConverter.php
@@ -5,6 +5,9 @@ namespace App\Commerce\Converter;
 use Siemendev\Checkout\Data\CheckoutDataInterface;
 use Siemendev\Checkout\Finalize\CheckoutFinalizationHandlerInterface;
 use Siemendev\Checkout\Finalize\CheckoutFinalizerInterface;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 /**
  * Demo of a checkout persistence handler that saves the checkout data to a file.
@@ -20,8 +23,21 @@ class CheckoutConverter implements CheckoutFinalizationHandlerInterface
 
     public function finalize(CheckoutDataInterface $data): void
     {
-        $data->orderFileName = 'var/orders/' . date('Y-m-d_H-i-s') . '.json';
-        file_put_contents($data->orderFileName, json_encode($data, JSON_PRETTY_PRINT));
+        $directory = '../var/orders/';
+        if (!is_dir($directory) && !mkdir(directory: $directory, recursive: true) && !is_dir($directory)) {
+            throw new \RuntimeException(sprintf('Directory "%s" was not created', $directory));
+        }
+        $data->orderFileName = date('Y-m-d_H-i-s') . '.xml';
+
+        $serializer = new Serializer([new ObjectNormalizer()], [new XmlEncoder()]);
+
+        file_put_contents(
+            $directory . $data->orderFileName,
+            $serializer->serialize($data, XmlEncoder::FORMAT, [
+                'xml_root_node_name' => 'order',
+                'xml_format_output' => true,
+            ]),
+        );
     }
 
     public function rollback(CheckoutDataInterface $data): void
