d3f4deb054d65d49a2923dd00e97cd8c593fb9d3
Patrick Siemen <post@patrick-siemen.de>
2023-06-07 15:14:17 +0200

    added documentation for rollbackCapture in PaymentMethodInterface

--- packages/payment/src/Method/PaymentMethodInterface.php
+++ packages/payment/src/Method/PaymentMethodInterface.php
@@ -22,6 +22,12 @@ interface PaymentMethodInterface
     public function capture(PaymentInterface $payment): void;
 
     /**
+     * Payment capture roll-back
+     * In case something goes wrong after the payment has been captured, we need to be able to roll back the capture.
+     * In case your payment gateway only supports rolling back the whole payment (so the payment is no longer
+     * authorized, not just the capture reverted) set $payment->setAuthorized(false) and the payment will automatically
+     * be removed from the checkout payments.
+     *
      * @throws PaymentCaptureRollbackException
      */
     public function rollbackCapture(PaymentInterface $payment): void;
