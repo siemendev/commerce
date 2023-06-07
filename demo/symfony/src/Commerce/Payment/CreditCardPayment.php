976265fbbbf3846cd947248bfbc925ab6f73e659
Patrick Siemen <post@patrick-siemen.de>
2023-06-07 13:51:01 +0200

    use getter/setter for CC Payment instead of public properties

--- demo/symfony/src/Commerce/Payment/CreditCardPayment.php
+++ demo/symfony/src/Commerce/Payment/CreditCardPayment.php
@@ -6,13 +6,108 @@ use Siemendev\Checkout\Payment\Payment\Payment;
 
 class CreditCardPayment extends Payment
 {
-    public string $cardNumber;
+    private string $cardNumber;
 
-    public string $cardHolder;
+    private string $cardHolder;
 
-    public int $cardExpiryMonth;
+    private int $cardExpiryMonth;
 
-    public int $cardExpiryYear;
+    private int $cardExpiryYear;
 
-    public string $cardCsc;
+    private string $cardCsc;
+
+    public function getPaymentMethodIdentifier(): string
+    {
+        return CreditCardPaymentMethod::IDENTIFIER;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCardNumber(): string
+    {
+        return $this->cardNumber;
+    }
+
+    /**
+     * @param string $cardNumber
+     * @return CreditCardPayment
+     */
+    public function setCardNumber(string $cardNumber): CreditCardPayment
+    {
+        $this->cardNumber = $cardNumber;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCardHolder(): string
+    {
+        return $this->cardHolder;
+    }
+
+    /**
+     * @param string $cardHolder
+     * @return CreditCardPayment
+     */
+    public function setCardHolder(string $cardHolder): CreditCardPayment
+    {
+        $this->cardHolder = $cardHolder;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCardExpiryMonth(): int
+    {
+        return $this->cardExpiryMonth;
+    }
+
+    /**
+     * @param int $cardExpiryMonth
+     * @return CreditCardPayment
+     */
+    public function setCardExpiryMonth(int $cardExpiryMonth): CreditCardPayment
+    {
+        $this->cardExpiryMonth = $cardExpiryMonth;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCardExpiryYear(): int
+    {
+        return $this->cardExpiryYear;
+    }
+
+    /**
+     * @param int $cardExpiryYear
+     * @return CreditCardPayment
+     */
+    public function setCardExpiryYear(int $cardExpiryYear): CreditCardPayment
+    {
+        $this->cardExpiryYear = $cardExpiryYear;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCardCsc(): string
+    {
+        return $this->cardCsc;
+    }
+
+    /**
+     * @param string $cardCsc
+     * @return CreditCardPayment
+     */
+    public function setCardCsc(string $cardCsc): CreditCardPayment
+    {
+        $this->cardCsc = $cardCsc;
+        return $this;
+    }
 }
