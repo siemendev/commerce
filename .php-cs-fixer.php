b8e29d62b15174014ff74e28b1047f80475dc93d
Patrick Siemen <post@patrick-siemen.de>
2023-06-16 11:08:17 +0200

    added cs-fixer configuration and Makefile target

---
+++ .php-cs-fixer.php
@@ -0,0 +1,39 @@
+<?php
+
+return (new PhpCsFixer\Config())
+    ->setUsingCache(false)
+    ->setFinder(
+        (new PhpCsFixer\Finder())
+            ->name('*.php')
+            ->ignoreDotFiles(true)
+            ->ignoreVCS(true)
+    )
+    ->setRiskyAllowed(true)
+    ->setRules([
+            '@PhpCsFixer' => true,
+            '@Symfony' => true,
+            '@PSR2' => true,
+            '@PER' => true,
+            'declare_strict_types' => true,
+            'global_namespace_import' => [
+                'import_classes' => true,
+                'import_constants' => true,
+                'import_functions' => false,
+            ],
+            'trailing_comma_in_multiline' => [
+                'after_heredoc' => true,
+                'elements' => ['arrays', 'arguments', 'parameters']
+            ],
+            'array_syntax' => ['syntax' => 'short'],
+            'phpdoc_align' => ['align' => 'left'],
+            'phpdoc_separation' => false,
+            'concat_space' => ['spacing' => 'one'],
+            'multiline_whitespace_before_semicolons' => ['strategy' => 'new_line_for_chained_calls'],
+            'class_attributes_separation' => ['elements' => ['const' => 'one', 'method' => 'one', 'property' => 'one']],
+            'phpdoc_line_span' => [
+                'const' => 'single',
+                'property' => 'single',
+                'method' => 'multi',
+            ],
+        ]
+    );
--- Makefile
+++ Makefile
@@ -10,7 +10,12 @@ update:
 
 phpstan:
 	for PACKAGE in packages/*; do make DIR=$$PACKAGE phpstan-package; done
-
 phpstan-package:
 	@echo "phpstan: $$DIR"
-	@cd $$DIR; phpstan analyse src --level 9 -a vendor/autoload.php || echo "phpstan failed, fix it and try it with: \"make DIR=$$DIR phpstan-package\""
+	@cd $$DIR; phpstan analyse src --level 9 -a vendor/autoload.php || echo "operation failed, fix it and try it with: \"make DIR=$$DIR phpstan-package\""
+
+cs-fixer:
+	for PACKAGE in packages/*; do make DIR=$$PACKAGE cs-fixer-package; done
+cs-fixer-package:
+	@echo "cs-fixer: $$DIR"
+	@php-cs-fixer fix $$DIR --config=.php-cs-fixer.php || echo "operation failed, fix it and try it with: \"make DIR=$$DIR cs-fixer-package\""
