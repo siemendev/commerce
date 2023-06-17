e19be58749f879866579c10f7a7bc1800376438f
Patrick Siemen <post@patrick-siemen.de>
2023-06-17 14:19:22 +0200

    fix credit card payment

--- demo/symfony/src/Controller/Payment/CreditCardPaymentController.php
+++ demo/symfony/src/Controller/Payment/CreditCardPaymentController.php
@@ -6,24 +6,23 @@ namespace App\Controller\Payment;
 
 use App\Commerce\Checkout;
 use App\Commerce\Payment\CreditCardPayment;
-use App\Controller\AbstractCheckoutController;
 use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/payment/credit-card', name: 'checkout.payment.credit-card')]
-class CreditCardPaymentController extends AbstractCheckoutController
+class CreditCardPaymentController extends AbstractController
 {
     /**
      * @throws Exception
      */
     public function __invoke(Request $request, Checkout $checkout): Response
     {
-        $data = $checkout->getCheckoutData();
+        $amount = $checkout->getCheckoutData()->getOpenTotal();
 
         $payment = (new CreditCardPayment())
-            ->setCapturedAmount($checkout->getCheckoutData()->getOpenTotal())
             ->setCurrency($checkout->getCheckoutData()->getCurrency())
             ->setCardNumber($request->request->get('card_number'))
             ->setCardHolder($request->request->get('card_holder'))
@@ -33,11 +32,12 @@ class CreditCardPaymentController extends AbstractCheckoutController
         ;
 
         // here is where you usually would start by authorizing the credit card payment
-        // $externalPaymentId = $externalPaymentProvider->authorize($externalPaymentId, $openTotal);
+        // $externalPaymentId = $externalPaymentProvider->authorize($externalPaymentId, $amount);
         $externalPaymentId = (string) rand(100000, 999999); // id mocked for now
 
         $payment
             ->setIdentifier($externalPaymentId)
+            ->setAuthorizedAmount($amount)
             ->setAuthorized(true)
         ;
         $checkout
@@ -45,6 +45,6 @@ class CreditCardPaymentController extends AbstractCheckoutController
             ->addPayment($payment)
         ;
 
-        return $this->redirectToCurrentStep();
+        return $this->redirectToRoute('checkout_payment');
     }
 }
