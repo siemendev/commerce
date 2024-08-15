9567181f3a4904dcda065e6b8cd0e54c7cf4801e
Patrick Siemen <post@patrick-siemen.de>
2024-08-15 20:49:10 +0200

    upgrade symfony demo project

--- demo/symfony/config/bundles.php
+++ demo/symfony/config/bundles.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
@@ -10,7 +8,6 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Siemendev\Checkout\SymfonyBridge\CheckoutBundle::class => ['all' => true],
     Siemendev\Checkout\Products\SymfonyBridge\CheckoutProductsBundle::class => ['all' => true],
     Siemendev\Checkout\Payment\SymfonyBridge\CheckoutPaymentBundle::class => ['all' => true],
